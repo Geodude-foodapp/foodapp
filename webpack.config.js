b9ebe8d55e3445869c78ba503d428b0e4fd1ae92
neighbor-peace <mcostello91@gmail.com>
2022-12-19 14:56:42 -0800

    init app
    Co-authored-by: Steven Geiger <73615329+geistnine@users.noreply.github.com>
    Co-authored-by: Melanie Forbes <50688150+mforbz12@users.noreply.github.com>
    Co-authored-by: Cedar Cooper <87053156+CedarCooper@users.noreply.github.com>
    Co-authored-by: Michael Costello <86714629+neighbor-peace@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: '/*',
+    },
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: [path.join(__dirname, './node_modules')],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './index.html',
+    }),
+  ],
+};

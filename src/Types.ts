8121af41e8a210242ccaf8d11074fda16d7ed082
neighbor-peace <mcostello91@gmail.com>
2022-12-20 15:28:31 -0800

    add diet, intolerance, and formState types
    Co-authored-by: Steven Geiger <73615329+geistnine@users.noreply.github.com>
    Co-authored-by: Melanie Forbes <50688150+mforbz12@users.noreply.github.com>
    Co-authored-by: Cedar Cooper <87053156+CedarCooper@users.noreply.github.com>
    Co-authored-by: Michael Costello <86714629+neighbor-peace@users.noreply.github.com>

--- src/Types.ts
+++ src/Types.ts
@@ -1,5 +1,35 @@
+export type Diet =
+  | 'Gluten Free'
+  | 'Ketogenic'
+  | 'Vegetarian'
+  | 'Lacto-Vegetarian'
+  | 'Ovo-Vegetarian'
+  | 'Vegan'
+  | 'Pescetarian'
+  | 'Paleo'
+  | 'Primal'
+  | 'Low FODMAP'
+  | 'Whole30';
+
+export type Intolerance =
+  | 'Dairy'
+  | 'Egg'
+  | 'Gluten'
+  | 'Grain'
+  | 'Peanut'
+  | 'Seafood'
+  | 'Sesame'
+  | 'Shellfish'
+  | 'Soy'
+  | 'Sulfite'
+  | 'Tree Nut'
+  | 'Wheat';
+
 export type UserFormState = {
   username: string;
   password: string;
   // TODO: ADD RESTRICTIONS AND OTHER PROFILE INFO
+  diet: Diet;
+  intolerances: Intolerance[];
+  exclusions: string[];
 };
